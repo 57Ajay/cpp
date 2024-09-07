8639e8dc6c05ceb46fa3eae74df20c222fc19651
57Ajay <57ajay.u@gmail.com>
2024-09-07 12:47:04 +0530

    charIdx more

--- charIdx.cpp
+++ charIdx.cpp
@@ -6,11 +6,20 @@ int charIndex(string str, char s){
 	int index;
 	for (int i = 0; i < str.length(); i++){
 		if (str.at(i) == s){
-			index = i;		
+			index = i;
+			break;
 	    };
 	};
 	return index;
 };
+
+string charIndexEfficient(string str, char s){
+	size_t index = str.find(s);
+	if (index == string::npos) {
+        return "Character '" + string(1, s) + "' not found in the string.";
+    };
+	return "The index of '" + string(1,s) + "' is at " + to_string(index);
+}
 
 int main (){
 	string str;
@@ -20,6 +29,7 @@ int main (){
     cout << "Enter the charecter to find it's index: ";
 	cin >> s;
 	int idx = charIndex(str, s);
-	cout << "The index of " << s << " is at " << idx << endl;
+	cout << "charIndex: => The index of " << s << " is at " << idx << endl;
+	cout << "chatIndexEfficient: =>" << charIndexEfficient(str, s);
 	return 0;
 }
