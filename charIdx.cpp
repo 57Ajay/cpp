c167fd71c7f2ca2b0f1c134a958d31cc7e6d8d47
57Ajay <57ajay.u@gmail.com>
2024-09-07 13:01:16 +0530

    added execution timer

--- charIdx.cpp
+++ charIdx.cpp
@@ -1,35 +1,53 @@
 #include <iostream>
 #include <string>
+#include <chrono>
+
 using namespace std;
 
-int charIndex(string str, char s){
-	int index;
-	for (int i = 0; i < str.length(); i++){
-		if (str.at(i) == s){
-			index = i;
-			break;
-	    };
-	};
-	return index;
-};
-
-string charIndexEfficient(string str, char s){
-	size_t index = str.find(s);
-	if (index == string::npos) {
-        return "Character '" + string(1, s) + "' not found in the string.";
-    };
-	return "The index of '" + string(1,s) + "' is at " + to_string(index);
+int charIndex(string str, char s) {
+    int index;
+    for (int i = 0; i < str.length(); i++) {
+        if (str.at(i) == s) {
+            index = i;
+            break;
+        }
+    }
+    return index;
 }
 
-int main (){
-	string str;
-	char s;
-	cout << "Enter the string: ";
-	cin >> str;
-    cout << "Enter the charecter to find it's index: ";
-	cin >> s;
-	int idx = charIndex(str, s);
-	cout << "charIndex: => The index of " << s << " is at " << idx << endl;
-	cout << "chatIndexEfficient: =>" << charIndexEfficient(str, s);
-	return 0;
+string charIndexEfficient(string str, char s) {
+    size_t index = str.find(s);
+    if (index == string::npos) {
+        return "Character '" + string(1, s) + "' not found in the string.";
+    }
+    return "The index of '" + string(1, s) + "' is at " + to_string(index);
+}
+
+int main() {
+    string str;
+    char s;
+
+    cout << "Enter the string: ";
+    cin >> str;
+    cout << "Enter the character to find its index: ";
+    cin >> s;
+
+    //Execution time for charIndex
+    auto start_time_charIndex = chrono::high_resolution_clock::now();
+    int idx = charIndex(str, s);
+    auto end_time_charIndex = chrono::high_resolution_clock::now();
+    double elapsed_time_charIndex = chrono::duration_cast<chrono::microseconds>(end_time_charIndex - start_time_charIndex).count() / 1e6;
+
+    //Execution time for charIndexEfficient
+    auto start_time_efficient = chrono::high_resolution_clock::now();
+    string result = charIndexEfficient(str, s);
+    auto end_time_efficient = chrono::high_resolution_clock::now();
+    double elapsed_time_efficient = chrono::duration_cast<chrono::microseconds>(end_time_efficient - start_time_efficient).count() / 1e6;
+
+    cout << "charIndex: => The index of " << s << " is at " << idx << endl;
+    cout << "charIndex time: " << elapsed_time_charIndex << " milliseconds" << endl;
+    cout << "charIndexEfficient: => " << result << endl;
+    cout << "charIndexEfficient time: " << elapsed_time_efficient << " milliseconds" << endl;
+
+    return 0;
 }
