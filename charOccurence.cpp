4fdbc76f18b43a1d73974455d0df1aaa751bd409
57Ajay <57ajay.u@gmail.com>
2024-09-08 22:28:06 +0530

    charOcrruence2

--- charOccurence.cpp
+++ charOccurence.cpp
@@ -12,9 +12,21 @@ int charOcr(string str, char c) {
   return count;
 }
 
+int wordOcr(string str, string word) {
+  int count = 0;
+  size_t pos = str.find(word);
+
+  while (pos != string::npos) {
+    count++;
+    pos = str.find(word, pos + word.length());
+  }
+
+  return count;
+}
+
 int main() {
   string str;
-  char c;
+  string input;
 
   cout << "Please enter a word or a sentence: => \n";
   getline(cin, str);
@@ -24,17 +36,19 @@ int main() {
     return 1;
   }
 
-  cout << "Please enter a character: => \n";
-  cin >> c;
+  cout << "Please enter a character or a word: => \n";
+  getline(cin, input);
 
-  if (cin.fail() || cin.peek() != '\n') {
-    cout << "Please enter a single character." << endl;
-    return 1;
+  if (input.length() == 1) {
+    char c = input[0];
+    int result = charOcr(str, c);
+    cout << "The character '" << c << "' occurs " << result
+         << " times in the string." << endl;
+  } else {
+    int result = wordOcr(str, input);
+    cout << "The word '" << input << "' occurs " << result
+         << " times in the string." << endl;
   }
-
-  int result = charOcr(str, c);
-  cout << "The character '" << c << "' occurs " << result
-       << " times in the string." << endl;
 
   return 0;
 }
