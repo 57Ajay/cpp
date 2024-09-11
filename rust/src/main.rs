40e5660f3a380f5f56a14075c017244d3bac1586
57Ajay <57ajay.u@gmail.com>
2024-09-12 00:23:51 +0530

    tooRusty againx2

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,5 @@
+use std::u8;
+// function
 fn fib(num: u32) -> u32 {
     if num == 0 {
         return 0;
@@ -14,12 +16,87 @@ fn fib(num: u32) -> u32 {
     }
     return curr;
 }
-
+//string
 fn str_len(s: &str) -> usize {
     return s.chars().count();
+}
+//struct
+struct User {
+    active: bool,
+    name: String,
+    password: String,
+    age: i8,
+}
+
+struct Rect {
+    length: u8,
+    bredth: u8,
+}
+//struct implementatin;
+impl Rect {
+    fn area(&self) -> u8 {
+        return self.length * self.bredth;
+    }
+    fn perimeter(&self, i: u8) -> u8 {
+        if i != 2 {
+            println!("Please enter only 2 an a parameter to find the perimeter of the rectangle.");
+            return 0;
+        }
+        i * (self.length + self.bredth)
+    }
+    fn polygon() -> bool {
+        return true;
+    } // this is a static function
+}
+
+//Enums
+
+enum Shape {
+    Circle(f32),
+    Square(f32),
+    Rectangle(f32, f32),
+}
+
+fn area(shape: Shape) -> f32 {
+    match shape {
+        Shape::Circle(r) => 3.14 * r * r,
+        Shape::Square(a) => a * a,
+        Shape::Rectangle(a, b) => a * b,
+    }
 }
 
 fn main() {
     println!("{}", fib(11));
-    println!("the length of the string is: {}", str_len("Ajay is here"))
+    println!("the length of the string is: {}", str_len("Ajay Upadhyay"));
+    let user = User {
+        active: true,
+        name: String::from("Ajay"),
+        password: String::from("qwer4321"),
+        age: 22,
+    };
+    println!(
+        "age: {}, name: {}, password: {}, active: {}",
+        user.age, user.name, user.password, user.active
+    );
+
+    let rectangle: Rect = Rect {
+        length: 12,
+        bredth: 21,
+    };
+
+    println!(
+        "area of the rectangle is: {}, perimeter of rectangle is: {}, is rectangle a polygon?: {}",
+        rectangle.area(),
+        rectangle.perimeter(3),
+        Rect::polygon()
+    );
+    let rect = Shape::Rectangle(12.0, 89.0);
+    let sq = Shape::Square(7.0);
+    let cir = Shape::Circle(49.0);
+    println!(
+        "Area of rectangle: {}, Area of square: {}, Area of circle: {}",
+        area(rect),
+        area(sq),
+        area(cir)
+    )
 }
