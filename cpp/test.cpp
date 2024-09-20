683c1be3e10529d9a9c3031d0a6959bec899ed8d
57Ajay <57ajay.u@gmail.com>
2024-09-20 22:07:05 +0530

    test

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,14 +1,37 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main() {
+int reverse_num(int num){
+	int num_copy = num;
+	int rev_num = 0;
+	while (num){
+		rev_num = rev_num*10 + num%10;
+		num /= 10;
+	}
+	cout << "The reverse on the number " << num_copy << " is ";
+	return rev_num;
+}
+int main(){
+	
+	int num;
+	cout << "Enter the number to be reversed: ";
+	cin >> num;
+	cout << reverse_num(num) << endl;
+	int arr[] = {2, 4, 6, 7, 1, 9};
+   	vector<int> new_arr;
 
-  int n = 5;
-  for (int i = 1; i <= 5; i++) {
-    for (int j = 1; j <= i; j++) {
-      cout << j;
-    }
-    cout << endl;
-  }
-  return 0;
+        for (int i : arr) {
+        	cout << i / 7 << endl;
+        	new_arr.push_back(i * 7);
+    	}
+
+   
+  	cout << "new_arr: "<< "[";
+   	for (int i : new_arr) {
+        	cout << i << " ";
+    	}
+	cout << "]";
+   	cout << endl;
+	return 0;
 }
